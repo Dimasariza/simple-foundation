91d2550b5cc0a99b1947e06403f474cddc392b6d
dimasariza <dimas.ariza20@gmail.com>
2024-08-28 22:21:31 +0700

    change db.json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const jsonServer  = require('json-server')
 const server      = jsonServer.create()
 // const router      = jsonServer.router(require('./db.json')())
-const router      = jsonServer.router('./public/demo/data/db.json')
+const router      = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
