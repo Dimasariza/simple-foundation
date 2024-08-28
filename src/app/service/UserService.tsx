e8ccce8d0b1a5d8593e034f0513d48488b7ff2a3
dimasariza <dimas.ariza20@gmail.com>
2024-08-28 22:39:39 +0700

    update user service

--- src/app/service/UserService.tsx
+++ src/app/service/UserService.tsx
@@ -4,20 +4,20 @@ import { IUser } from "../types/user";
 const endPoint = 'https://simple-foundation.vercel.app/api/user';
 
 export const UserService = {
-    getUsers() {
-      return axios.get(endPoint)
+    async getUsers() {
+      return await axios.get(endPoint)
       .then(({data}) => data as IUser[])
     },
-    getUserById(id: string | number) {
-      return axios.get(endPoint + "/" + id)
+    async getUserById(id: string | number) {
+      return await axios.get(endPoint + "/" + id)
       .then(({data}) => data as IUser)
     },
-    updateUser(id: string | number) {
-      return axios.put(endPoint + "/" + id)
+    async updateUser(id: string | number) {
+      return await axios.put(endPoint + "/" + id)
       .then(({data}) => data as IUser)
     },
-    deleteUser(id: string | number) {
-      return axios.delete(endPoint + "/" + id)
+    async deleteUser(id: string | number) {
+      return await axios.delete(endPoint + "/" + id)
       .then(({data}) => data as IUser)
     }
 };
